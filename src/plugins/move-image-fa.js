2bf32222fcfff0d60e2665ae03b10cb5bd6e1473
pvckstein <160131995+pvckstein@users.noreply.github.com>
2025-09-28 17:18:08 +0200

    Update move-image-fa.js

--- src/plugins/move-image-fa.js
+++ src/plugins/move-image-fa.js
@@ -1,23 +1,30 @@
-(function(root, factory){
-  if (typeof module === 'object' && module.exports) module.exports = factory();
-  else (root.GoblinQueue = root.GoblinQueue || []).push(factory());
-}(typeof self !== 'undefined' ? self : this, function(){
+(function (root, factory) {
+  'use strict';
+  var plugin = factory();
+
+  // ✅ Si Goblin ya existe, registra el plugin ahora mismo
+  if (typeof window !== 'undefined' && window.Goblin && typeof window.Goblin.use === 'function') {
+    window.Goblin.use(plugin);
+  } else {
+    // Fallback: encola el plugin para cuando cargue el core
+    (root.GoblinQueue = root.GoblinQueue || []).push(plugin);
+  }
+}(typeof self !== 'undefined' ? self : this, function () {
   'use strict';
 
   const NAME = 'move-image-and-links-fa';
 
   function moveOneForum(forum, utils) {
-    // 1) Mover .forum_links / .forum-links → .links
+    // 1) .forum_links / .forum-links → .links
     const linksSrc = forum.querySelector('.forum_links, .forum-links');
     const linksDst = forum.querySelector('.links');
     if (linksSrc && linksDst && utils.once(linksSrc, 'moved-links')) {
       linksDst.appendChild(linksSrc);
-      // opcional: eliminar <br> suelto que quedara en la descripción
       const br = forum.querySelector('.descripcion > br');
       if (br) br.remove();
     }
 
-    // 2) Mover primera imagen de .descripcion → .forumimg
+    // 2) primera imagen de .descripcion → .forumimg
     const img = forum.querySelector('.descripcion img');
     const imgDst = forum.querySelector('.forumimg');
     if (img && imgDst && utils.once(img, 'moved-img')) {
@@ -29,7 +36,10 @@
     name: NAME,
     idempotent: true,
     run({ utils, ctx }) {
-      utils.qsa('.forum', ctx).forEach(f => moveOneForum(f, utils));
+      const forums = utils.qsa('.forum', ctx);
+      // Log de ayuda (puedes quitarlo luego)
+      console.log('[Goblin] move-image-fa running on', forums.length, 'forums');
+      forums.forEach(f => moveOneForum(f, utils));
     }
   };
 }));
