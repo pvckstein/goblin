b531b46228a3e6970e3b6a8b72256eaefd4a4b4f
pvckstein <160131995+pvckstein@users.noreply.github.com>
2025-09-28 18:51:14 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import './core/goblin.core.js';
 import moveImageFA from './plugins/move-image-fa.js';
 import addIds from './plugins/add-ids.js';
 import topicPrefixes from './plugins/topic-prefixes.js';
+import codeboxCopy   from './plugins/codebox-copy.js';
 
 if (window && window.Goblin) {
   const { Goblin } = window;
   Goblin.use(moveImageFA);
   Goblin.use(addIds);
   Goblin.use(topicPrefixes);
+  Goblin.use(codeboxCopy); 
   Goblin.start();
 }
