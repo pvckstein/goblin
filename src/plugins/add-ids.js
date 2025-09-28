d20a43292921efd87a895749ce3b9cf471a2f01c
pvckstein <160131995+pvckstein@users.noreply.github.com>
2025-09-28 17:23:53 +0200

    Update add-ids.js

--- src/plugins/add-ids.js
+++ src/plugins/add-ids.js
@@ -1,18 +1,13 @@
-(function (root, factory) {
-  if (typeof module === 'object' && module.exports) module.exports = factory();
-  else (root.GoblinQueue = root.GoblinQueue || []).push(factory());
-}(typeof self !== 'undefined' ? self : this, function () {
-  'use strict';
+// ESM: exporta el plugin; no usa GoblinQueue
+const plugin = {
+  name: 'add-forum-ids',
+  idempotent: true,
+  run({ utils, ctx }) {
+    const items = utils.qsa('.forum', ctx);
+    items.forEach((el, i) => {
+      if (!el.id) el.id = 'f' + i;
+    });
+  }
+};
 
-  const NAME = 'add-forum-ids';
-
-  return {
-    name: NAME,
-    run({ utils, ctx }) {
-      const items = utils.qsa('.forum', ctx);
-      items.forEach((el, i) => {
-        if (!el.id) el.id = 'f' + i;
-      });
-    }
-  };
-}));
+export default plugin;
