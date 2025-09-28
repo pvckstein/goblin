5cfa4cf2130d7753e36c4187c0b25183566ddec0
pvckstein <160131995+pvckstein@users.noreply.github.com>
2025-09-28 17:02:47 +0200

    Update move-image.js

--- src/plugins/move-image.js
+++ src/plugins/move-image.js
@@ -1,20 +1,23 @@
-(function (root, factory) {
+(function(root, factory){
   if (typeof module === 'object' && module.exports) module.exports = factory();
   else (root.GoblinQueue = root.GoblinQueue || []).push(factory());
-}(typeof self !== 'undefined' ? self : this, function () {
+}(typeof self !== 'undefined' ? self : this, function(){
   'use strict';
 
-  const NAME = 'move-image-and-links';
+  const NAME = 'move-image-and-links-fa';
 
-  function moveOneForum(forum, { utils }) {
-    // Mover .forum-links → .links
-    const linksSrc = forum.querySelector('.forum-links');
+  function moveOneForum(forum, utils) {
+    // 1) Mover .forum_links / .forum-links → .links
+    const linksSrc = forum.querySelector('.forum_links, .forum-links');
     const linksDst = forum.querySelector('.links');
     if (linksSrc && linksDst && utils.once(linksSrc, 'moved-links')) {
       linksDst.appendChild(linksSrc);
+      // opcional: eliminar <br> suelto que quedara en la descripción
+      const br = forum.querySelector('.descripcion > br');
+      if (br) br.remove();
     }
 
-    // Mover primera imagen de .descripcion → .forumimg
+    // 2) Mover primera imagen de .descripcion → .forumimg
     const img = forum.querySelector('.descripcion img');
     const imgDst = forum.querySelector('.forumimg');
     if (img && imgDst && utils.once(img, 'moved-img')) {
@@ -24,9 +27,9 @@
 
   return {
     name: NAME,
-    run({ doc, utils, ctx }) {
-      if (!doc) return;
-      utils.qsa('.forum', ctx).forEach(f => moveOneForum(f, { utils }));
+    idempotent: true,
+    run({ utils, ctx }) {
+      utils.qsa('.forum', ctx).forEach(f => moveOneForum(f, utils));
     }
   };
 }));
