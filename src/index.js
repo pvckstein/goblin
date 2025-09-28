18f7b199f75c27929544f04923cee0ebca34fd2c
pvckstein <160131995+pvckstein@users.noreply.github.com>
2025-09-28 17:35:13 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
-import './core/goblin.core.js';            // define window.Goblin (UMD)
+import './core/goblin.core.js';
+
 import moveImageFA from './plugins/move-image-fa.js';
 import addIds from './plugins/add-ids.js';
+import topicPrefixes from './plugins/topic-prefixes.js';
 
-// Registro explícito: ESM-friendly (sin GoblinQueue)
 if (window && window.Goblin) {
-  window.Goblin.use(moveImageFA);
-  window.Goblin.use(addIds);
-  window.Goblin.start();
+  const { Goblin } = window;
+  Goblin.use(moveImageFA);
+  Goblin.use(addIds);
+  Goblin.use(topicPrefixes);
+  Goblin.start();
 }
