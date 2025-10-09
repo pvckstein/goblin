03fa612143bdd685eab77fa948faeb9435b77b98
pvckstein <160131995+pvckstein@users.noreply.github.com>
2025-10-09 09:12:44 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,11 +5,13 @@ import addIds from './plugins/add-ids.js';
 import topicPrefixes from './plugins/topic-prefixes.js';
 import codeboxCopy   from './plugins/codebox-copy.js';
 import diceRollMap from './plugins/dice-rollmap.js';
+import categoryCols   from './plugins/category-columns.js'; 
 
 if (window && window.Goblin) {
   const { Goblin } = window;
   Goblin.use(moveImageFA);
   Goblin.use(addIds);
+  Goblin.use(categoryCols);
   Goblin.use(topicPrefixes);
   Goblin.use(codeboxCopy); 
   Goblin.use(diceRollMap);
